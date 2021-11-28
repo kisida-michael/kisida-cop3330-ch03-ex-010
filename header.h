7e3f433d88c82c3cee6c272213aff5cdd2acca4e
kisida-michael <michael@kisida.com>
2021-11-28 00:20:19 -0500

    oops

---
+++ header.h
@@ -0,0 +1,13 @@
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Michael Kisida
+ */
+
+#include <iostream>
+using namespace std;
+
+double mul(char, double, double);
+double div(char, double, double);
+double sub(char, double, double);
+double add(char, double, double);
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "header.h"
+
+int main(){
+    double num1;
+    double num2;
+    char op;
+
+    cin >> op; 
+
+
+switch(op) {
+  case '+':
+    cout <<"PLus" ;
+    break;
+  case '-':
+     cout <<"subtract" ;
+    break;
+  default:
+    // code block
+}
+
+    return 0;
+}
